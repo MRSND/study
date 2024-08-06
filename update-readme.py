0877dba152fa97589110a46e7351ac862935a465
xxhyxxnjxn <hyunjin077@naver.com>
2024-08-06 17:40:14 +0900

    update-readme.py 생성

---
+++ update-readme.py
@@ -0,0 +1,17 @@
+readme_file_path = "./README.md"
+
+# update readme file
+readme = open(readme_file_path, "w")
+readme.write('<div align="center">' +"\n \n")
+readme.write('  # Study' +"\n \n")
+readme.write('<img width="300px" height="300px" src="https://github.com/user-attachments/assets/a83c6d10-ce1f-4294-9519-1db4f336294f"/>' +"\n \n")
+readme.write('모각코' + "\n \n")
+readme.write(' |<img src="https://github.com/MRSND/study/assets/43220356/c3ba9fc4-4970-4ad1-9eef-05d4fea55043" width="100px" height="100px" title="Github_Logo"/>|<img src="https://github.com/MRSND/study/assets/43220356/ffd80c82-ccf1-469a-b233-a43a3bcb74af" width="100px" height="100px" title="Github_Logo"/>|<img src="https://github.com/MRSND/study/assets/43220356/dd123f9c-b4ac-44b2-b6fe-834ebe58463b" width="100px" height="100px" title="Github_Logo"/>|' +"\n"
+    + '|-----------|-------------|----------------------------------------|'+"\n"
+    + '| [@sycap01](https://github.com/sycap01) | [@xxhyxxnjxn](https://github.com/xxhyxxnjxn) | [@h1un](https://github.com/h1un)|' + "\n"
+    + '| - 개발서적 학습/실습<br/> - spring, 금융업무관련 지식습득   | - 공식문서 읽기 및 실습<br/>    - spring security, jwt, oauth2          | - React 공식문서 읽기<br/> - x 닷컴 클론코딩 |' + "\n \n")
+readme.write('## 스터디 일지' + "\n \n")
+readme.write(' [2024년 07월 10일](https://github.com/MRSND/study/wiki/2024%E2%80%9007%E2%80%9010)' +"\n \n")
+readme.write(' [2024년 07월 24일](https://github.com/MRSND/study/wiki/2024%E2%80%9007%E2%80%9024)' +"\n \n")
+readme.write(' [2024년 07월 31일](https://github.com/MRSND/study/wiki/2024%E2%80%9007%E2%80%9031)' +"\n \n")
+readme.close()
